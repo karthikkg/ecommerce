b4ef3838abbbbdf8acf2232899347c53f68cb1a2
Karthik G <karthik.katy@gmail.com>
2018-01-29 23:09:15 +0530

    fix byte problem

--- microservices/app/src/hasura.py
+++ microservices/app/src/hasura.py
@@ -452,9 +452,9 @@ def getPhoto_url(file):
 
 @hasura_examples.route('/add_product',methods=['GET','POST'])
 def add_product():
-    user_info = getinfo()
-    print('entered add product \n',getinfo())
-    print("the return type getinfo() is", type(getinfo()))
+    #user_info = getinfo()
+    #print('entered add product \n',getinfo())
+    #print("the return type getinfo() is", type(getinfo()))
     #return type(getinfo)
     if request.method == "POST" and '_flashes' in session:
         hasura_id= session['_flashes'][0][1]['hasura_id']
