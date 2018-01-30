9f64bdee6c81b039c70d3f32b400210eac1061f3
Karthik G <karthik.katy@gmail.com>
2018-01-30 18:07:50 +0530

    get userinfo

--- microservices/app/src/hasura.py
+++ microservices/app/src/hasura.py
@@ -413,7 +413,16 @@ def login():
         # Make the query and store response in resp
         resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
         response = make_response(render_template('index.html'))
-        response.set_cookie(resp.content)
+        string = resp.content.decode('utf-8')
+        json_obj = json.loads(string)
+        print(json_obj)
+        session_tokens = json_obj
+        
+        #session_tokens = resp.content.decode('utf8')
+        for i in session_tokens:
+            response.set_cookie(i, session_tokens[i])
+
+
         #response.set_cookie('age', b'26')
         # resp.content contains the json response.
         print(resp.content)
