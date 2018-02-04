7e70bf274d80093b0739347e1be0a3bdf23756e3
Karthik G <karthik.katy@gmail.com>
2018-02-04 15:51:50 +0530

    add react native frontend

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -2,12 +2,3 @@ from src import app
 # from flask import jsonify
 
 
-@app.route("/")
-def home():
-    return "Hasura Hello World"
-
-# Uncomment to add a new URL at /new
-
-# @app.route("/json")
-# def json_message():
-#     return jsonify(message="Hello World")
