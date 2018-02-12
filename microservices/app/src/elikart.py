1b7af8a175723d0a61f8ded56fc9da43114a0232
Karthik G <karthik.katy@gmail.com>
2018-02-13 01:55:07 +0530

    set session when logout

--- microservices/app/src/elikart.py
+++ microservices/app/src/elikart.py
@@ -372,7 +372,9 @@ def login():
         #print(json_obj)
         #session_tokens = json_obj
         session_tokens = resp.content.decode('utf8')
-        session_tokens = literal_eval(session_tokens)[0]
+        print('resp.content.decode\n',session_tokens)
+        session_tokens = literal_eval(session_tokens)
+        print('\nsession_tokens\n',session_tokens)
 
         
         for i in session_tokens:
